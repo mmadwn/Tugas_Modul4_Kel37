9e1c06a920e2553702ecf01943549e2353b12e22
- Ridwan <49033429+mmadwn@users.noreply.github.com>
2021-09-19 17:40:14 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import useJsonbox from "./hooks/useJsonbox";
+import Form from "./components/form";
+import FormUrl from "./components/form-url";
+import Item from "./components/item";
+import Notice from "./components/notice";
+import Footer from "./components/footer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// IMPORTANT
+// Get your JsonBox on https://jsonbox.io and assign your url to `yourJsonboxUrl` bellow
+const yourJsonboxUrl = "";
+
+function App() {
+  const [jsonboxUrl, setJsonboxUrl] = useState(yourJsonboxUrl);
+  const { messages, isLoading, setNewMessage, setDelMessage } = useJsonbox(
+    jsonboxUrl
+  );
+  return (
+    <div className="App">
+      <h1>Tugas MODUL 4 Kelompok 37</h1>
+
+      {jsonboxUrl ? (
+        <>
+          <Form setNewMessage={setNewMessage} />
+          <div>
+            {!isLoading && messages.length
+              ? messages.map(item => (
+                  <Item
+                    key={item._id}
+                    item={item}
+                    setDelMessage={setDelMessage}
+                  />
+                ))
+              : ""}
+            {isLoading && <code>Waiting...</code>}
+            {!isLoading && !messages.length && (
+              <em>
+                <code>No Message yet...</code>
+              </em>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <FormUrl setJsonboxUrl={setJsonboxUrl} />
+          <hr />
+          <Notice />
+        </>
+      )}
+      <Footer jsonboxUrl={jsonboxUrl} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
